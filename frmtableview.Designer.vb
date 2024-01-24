6288ae5d8085f450a1d56c5af1a1e9ca127603c1
JoyangAR <joacotosso@gmail.com>
2024-01-24 16:10:49 -0300

    Added Table View frm to load & see a database's tables & table's rows

---
+++ frmtableview.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmtableview
+    Inherits System.Windows.Forms.Form
+
+    ' Form replaces Dispose to clean up the list of components.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    ' Required by the Windows Forms Designer
+    Private components As System.ComponentModel.IContainer
+
+    ' NOTE: The Windows Forms Designer needs the following procedure
+    ' It can be modified using the Windows Forms Designer.  
+    ' Do not modify it with the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dtgridtable = New System.Windows.Forms.DataGridView()
+        Me.lsttables = New System.Windows.Forms.ListBox()
+        Me.txtrows = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.dtgridtable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dtgridtable
+        '
+        Me.dtgridtable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtgridtable.Location = New System.Drawing.Point(165, 12)
+        Me.dtgridtable.Name = "dtgridtable"
+        Me.dtgridtable.Size = New System.Drawing.Size(623, 433)
+        Me.dtgridtable.TabIndex = 1
+        '
+        'lsttables
+        '
+        Me.lsttables.FormattingEnabled = True
+        Me.lsttables.Location = New System.Drawing.Point(12, 12)
+        Me.lsttables.Name = "lsttables"
+        Me.lsttables.Size = New System.Drawing.Size(147, 407)
+        Me.lsttables.TabIndex = 2
+        '
+        'txtrows
+        '
+        Me.txtrows.Location = New System.Drawing.Point(98, 425)
+        Me.txtrows.Name = "txtrows"
+        Me.txtrows.Size = New System.Drawing.Size(61, 20)
+        Me.txtrows.TabIndex = 3
+        Me.txtrows.Text = "1000"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 428)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Amount of rows"
+        '
+        'frmtableview
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 455)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtrows)
+        Me.Controls.Add(Me.lsttables)
+        Me.Controls.Add(Me.dtgridtable)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(820, 498)
+        Me.MinimumSize = New System.Drawing.Size(820, 498)
+        Me.Name = "frmtableview"
+        Me.ShowIcon = False
+        Me.Text = "Table View"
+        CType(Me.dtgridtable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dtgridtable As DataGridView
+    Friend WithEvents lsttables As ListBox
+    Friend WithEvents txtrows As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ frmtableview.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Collections.Generic
+
+Public Class frmtableview
+
+    Private selectedDatabase As String = frmmain.lstdb.SelectedItem.ToString()
+
+    Private Sub frmtableview_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Get table names for the selected database
+        Dim tableNames As List(Of String) = GetTableNames(selectedDatabase)
+
+        ' Populate the ListView with table names
+        lsttables.Items.Clear()
+        For Each tableName As String In tableNames
+            lsttables.Items.Add(tableName)
+        Next
+    End Sub
+
+    Private Sub lsttables_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lsttables.SelectedIndexChanged
+        ' Get the selected table and the number of rows from the controls
+        Dim selectedTable As String = lsttables.SelectedItem.ToString()
+        Dim numRows As Integer
+
+        If Not Integer.TryParse(txtrows.Text, numRows) Then
+            MessageBox.Show("Please enter a valid number for the number of rows.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Get the rows and display them in the DataGridView
+        Dim dataTable As DataTable = GetRows(selectedTable, numRows)
+        dtgridtable.DataSource = dataTable
+    End Sub
+End Class
