bb84a1159c03183b7a52411186be09dd7b57049c
JoyangAR <joacotosso@gmail.com>
2024-02-21 22:40:57 -0300

    Why did I deleted AssemblyInfo??

---
+++ AssemblyInfo.vb
@@ -0,0 +1,34 @@
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports System.Security.Principal
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+
+' TODO: Review the values of the assembly attributes
+
+
+<Assembly: AssemblyTitle("SQL Server Manager Lite")>
+<Assembly: AssemblyDescription("A simple database management tool for MS SQL Server")>
+<Assembly: AssemblyCompany("JoyangAR")>
+<Assembly: AssemblyProduct("SQL Server Manager Lite")>
+<Assembly: AssemblyCopyright("MIT License")>
+<Assembly: AssemblyTrademark("MS SQL Server is a trademark of Microsoft Corp.")>
+<Assembly: AssemblyCulture("")>
+
+' Version information for an assembly consists of the following four values:
+
+'	Major version
+'	Minor Version
+'	Build Number
+'	Revision
+
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.3.*")>
+
+
