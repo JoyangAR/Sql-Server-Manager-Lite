7d1c97fbb9d28ef9fee50982034a58b6e3e1ea14
JoyangAR <joacotosso@gmail.com>
2024-03-08 22:58:12 -0300

    Added config form

---
+++ frmconfig.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmconfig
+    Inherits System.Windows.Forms.Form
+    'Form overrides Dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Forms Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The Windows Forms Designer requires the following procedure
+    'It can be modified using the Windows Forms Designer.  
+    'Do not modify it with the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.chkautologin = New System.Windows.Forms.CheckBox()
+        Me.cmddataloc = New System.Windows.Forms.Button()
+        Me.CmdApply = New System.Windows.Forms.Button()
+        Me.chklogtofile = New System.Windows.Forms.CheckBox()
+        Me.chkcolourQE = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'chkautologin
+        '
+        Me.chkautologin.AutoSize = True
+        Me.chkautologin.Location = New System.Drawing.Point(12, 12)
+        Me.chkautologin.Name = "chkautologin"
+        Me.chkautologin.Size = New System.Drawing.Size(78, 17)
+        Me.chkautologin.TabIndex = 0
+        Me.chkautologin.Text = "Auto LogIn"
+        Me.chkautologin.UseVisualStyleBackColor = True
+        '
+        'cmddataloc
+        '
+        Me.cmddataloc.BackColor = System.Drawing.SystemColors.Control
+        Me.cmddataloc.Cursor = System.Windows.Forms.Cursors.Default
+        Me.cmddataloc.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.cmddataloc.Location = New System.Drawing.Point(96, 7)
+        Me.cmddataloc.Name = "cmddataloc"
+        Me.cmddataloc.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.cmddataloc.Size = New System.Drawing.Size(81, 25)
+        Me.cmddataloc.TabIndex = 24
+        Me.cmddataloc.Text = "Data location"
+        Me.cmddataloc.UseVisualStyleBackColor = False
+        '
+        'CmdApply
+        '
+        Me.CmdApply.BackColor = System.Drawing.SystemColors.Control
+        Me.CmdApply.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CmdApply.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.CmdApply.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.CmdApply.Location = New System.Drawing.Point(51, 91)
+        Me.CmdApply.Name = "CmdApply"
+        Me.CmdApply.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CmdApply.Size = New System.Drawing.Size(81, 25)
+        Me.CmdApply.TabIndex = 25
+        Me.CmdApply.Text = "Apply"
+        Me.CmdApply.UseVisualStyleBackColor = False
+        '
+        'chklogtofile
+        '
+        Me.chklogtofile.AutoSize = True
+        Me.chklogtofile.Location = New System.Drawing.Point(12, 35)
+        Me.chklogtofile.Name = "chklogtofile"
+        Me.chklogtofile.Size = New System.Drawing.Size(72, 17)
+        Me.chklogtofile.TabIndex = 26
+        Me.chklogtofile.Text = "Log to file"
+        Me.chklogtofile.UseVisualStyleBackColor = True
+        '
+        'chkcolourQE
+        '
+        Me.chkcolourQE.AutoSize = True
+        Me.chkcolourQE.Location = New System.Drawing.Point(12, 58)
+        Me.chkcolourQE.Name = "chkcolourQE"
+        Me.chkcolourQE.Size = New System.Drawing.Size(105, 17)
+        Me.chkcolourQE.TabIndex = 27
+        Me.chkcolourQE.Text = "Colour text in QE"
+        Me.chkcolourQE.UseVisualStyleBackColor = True
+        '
+        'frmconfig
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(189, 128)
+        Me.Controls.Add(Me.chkcolourQE)
+        Me.Controls.Add(Me.chklogtofile)
+        Me.Controls.Add(Me.CmdApply)
+        Me.Controls.Add(Me.cmddataloc)
+        Me.Controls.Add(Me.chkautologin)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmconfig"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Configuration"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents chkautologin As CheckBox
+    Public WithEvents cmddataloc As Button
+    Public WithEvents CmdApply As Button
+    Friend WithEvents chklogtofile As CheckBox
+    Friend WithEvents chkcolourQE As CheckBox
+End Class
---
+++ frmconfig.vb
@@ -0,0 +1,24 @@
+﻿Public Class frmconfig
+    Private Sub cmddataloc_Click(sender As Object, e As EventArgs) Handles cmddataloc.Click
+        Dim defaultDataPath As String = String.Empty
+        Dim defaultLogPath As String = String.Empty
+        If GetDefaultDataAndLogLocations(defaultDataPath, defaultLogPath) Then
+            frmfilespath.TxtMDF.Text = defaultDataPath
+            frmfilespath.TxtLDF.Text = defaultLogPath
+            frmfilespath.ShowDialog()
+        End If
+    End Sub
+
+    Private Sub CmdApply_Click(sender As Object, e As EventArgs) Handles CmdApply.Click
+        autologin = chkautologin.CheckState
+        logtofile = chklogtofile.Checked
+        colourQE = chkcolourQE.Checked
+        WriteXML()
+    End Sub
+
+    Private Sub frmconfig_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        chkautologin.Checked = autologin
+        chklogtofile.Checked = logtofile
+        chkcolourQE.Checked = colourQE
+    End Sub
+End Class
