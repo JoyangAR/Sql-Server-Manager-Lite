6e06b604703ece85e19413332c45decbc9bf7cb6
JoyangAR <joacotosso@gmail.com>
2024-03-23 23:02:17 -0300

    Reorganized frmconfig

--- src/frmconfig.Designer.vb
+++ src/frmconfig.Designer.vb
@@ -1,6 +1,7 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmconfig
     Inherits System.Windows.Forms.Form
+
     'Form overrides Dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
@@ -13,95 +14,56 @@ Partial Class frmconfig
         End Try
     End Sub
 
-    'Required by the Windows Forms Designer
+    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The Windows Forms Designer requires the following procedure
-    'It can be modified using the Windows Forms Designer.  
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
     'Do not modify it with the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.chkautologin = New System.Windows.Forms.CheckBox()
-        Me.cmddataloc = New System.Windows.Forms.Button()
-        Me.CmdApply = New System.Windows.Forms.Button()
-        Me.chklogtofile = New System.Windows.Forms.CheckBox()
-        Me.chkcolourQE = New System.Windows.Forms.CheckBox()
-        Me.chkdisablerandom = New System.Windows.Forms.CheckBox()
+        Me.grpSettings = New System.Windows.Forms.GroupBox()
         Me.chkUpdates = New System.Windows.Forms.CheckBox()
         Me.cmdCheckUpd = New System.Windows.Forms.Button()
+        Me.chkdisablerandom = New System.Windows.Forms.CheckBox()
+        Me.chklogtofile = New System.Windows.Forms.CheckBox()
+        Me.grpQE = New System.Windows.Forms.GroupBox()
+        Me.chkcolourQE = New System.Windows.Forms.CheckBox()
+        Me.CmdApply = New System.Windows.Forms.Button()
+        Me.cmddataloc = New System.Windows.Forms.Button()
+        Me.grpSettings.SuspendLayout()
+        Me.grpQE.SuspendLayout()
         Me.SuspendLayout()
         '
         'chkautologin
         '
         Me.chkautologin.AutoSize = True
-        Me.chkautologin.Location = New System.Drawing.Point(12, 12)
+        Me.chkautologin.Location = New System.Drawing.Point(6, 88)
         Me.chkautologin.Name = "chkautologin"
         Me.chkautologin.Size = New System.Drawing.Size(78, 17)
         Me.chkautologin.TabIndex = 0
         Me.chkautologin.Text = "Auto LogIn"
         Me.chkautologin.UseVisualStyleBackColor = True
         '
-        'cmddataloc
+        'grpSettings
         '
-        Me.cmddataloc.BackColor = System.Drawing.SystemColors.Control
-        Me.cmddataloc.Cursor = System.Windows.Forms.Cursors.Default
-        Me.cmddataloc.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.cmddataloc.Location = New System.Drawing.Point(135, 7)
-        Me.cmddataloc.Name = "cmddataloc"
-        Me.cmddataloc.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.cmddataloc.Size = New System.Drawing.Size(81, 25)
-        Me.cmddataloc.TabIndex = 24
-        Me.cmddataloc.Text = "Data location"
-        Me.cmddataloc.UseVisualStyleBackColor = False
-        '
-        'CmdApply
-        '
-        Me.CmdApply.BackColor = System.Drawing.SystemColors.Control
-        Me.CmdApply.Cursor = System.Windows.Forms.Cursors.Default
-        Me.CmdApply.DialogResult = System.Windows.Forms.DialogResult.OK
-        Me.CmdApply.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.CmdApply.Location = New System.Drawing.Point(86, 143)
-        Me.CmdApply.Name = "CmdApply"
-        Me.CmdApply.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.CmdApply.Size = New System.Drawing.Size(81, 25)
-        Me.CmdApply.TabIndex = 25
-        Me.CmdApply.Text = "Apply"
-        Me.CmdApply.UseVisualStyleBackColor = False
-        '
-        'chklogtofile
-        '
-        Me.chklogtofile.AutoSize = True
-        Me.chklogtofile.Location = New System.Drawing.Point(12, 35)
-        Me.chklogtofile.Name = "chklogtofile"
-        Me.chklogtofile.Size = New System.Drawing.Size(72, 17)
-        Me.chklogtofile.TabIndex = 26
-        Me.chklogtofile.Text = "Log to file"
-        Me.chklogtofile.UseVisualStyleBackColor = True
-        '
-        'chkcolourQE
-        '
-        Me.chkcolourQE.AutoSize = True
-        Me.chkcolourQE.Location = New System.Drawing.Point(12, 58)
-        Me.chkcolourQE.Name = "chkcolourQE"
-        Me.chkcolourQE.Size = New System.Drawing.Size(183, 17)
-        Me.chkcolourQE.TabIndex = 27
-        Me.chkcolourQE.Text = "Colour text in QE (To be repaired)"
-        Me.chkcolourQE.UseVisualStyleBackColor = True
-        '
-        'chkdisablerandom
-        '
-        Me.chkdisablerandom.AutoSize = True
-        Me.chkdisablerandom.Location = New System.Drawing.Point(12, 81)
-        Me.chkdisablerandom.Name = "chkdisablerandom"
-        Me.chkdisablerandom.Size = New System.Drawing.Size(127, 17)
-        Me.chkdisablerandom.TabIndex = 28
-        Me.chkdisablerandom.Text = "Disable RND security"
-        Me.chkdisablerandom.UseVisualStyleBackColor = True
+        Me.grpSettings.Controls.Add(Me.chkUpdates)
+        Me.grpSettings.Controls.Add(Me.cmdCheckUpd)
+        Me.grpSettings.Controls.Add(Me.chkdisablerandom)
+        Me.grpSettings.Controls.Add(Me.chklogtofile)
+        Me.grpSettings.Controls.Add(Me.chkautologin)
+        Me.grpSettings.Location = New System.Drawing.Point(12, 12)
+        Me.grpSettings.Name = "grpSettings"
+        Me.grpSettings.Size = New System.Drawing.Size(225, 132)
+        Me.grpSettings.TabIndex = 1
+        Me.grpSettings.TabStop = False
+        Me.grpSettings.Text = "Settings"
         '
         'chkUpdates
         '
         Me.chkUpdates.AutoSize = True
-        Me.chkUpdates.Location = New System.Drawing.Point(12, 104)
+        Me.chkUpdates.Location = New System.Drawing.Point(6, 19)
         Me.chkUpdates.Name = "chkUpdates"
         Me.chkUpdates.Size = New System.Drawing.Size(148, 17)
         Me.chkUpdates.TabIndex = 29
@@ -110,47 +72,104 @@ Partial Class frmconfig
         '
         'cmdCheckUpd
         '
-        Me.cmdCheckUpd.BackColor = System.Drawing.SystemColors.Control
-        Me.cmdCheckUpd.Cursor = System.Windows.Forms.Cursors.Default
-        Me.cmdCheckUpd.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.cmdCheckUpd.Location = New System.Drawing.Point(166, 99)
+        Me.cmdCheckUpd.Location = New System.Drawing.Point(147, 94)
         Me.cmdCheckUpd.Name = "cmdCheckUpd"
-        Me.cmdCheckUpd.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.cmdCheckUpd.Size = New System.Drawing.Size(71, 25)
         Me.cmdCheckUpd.TabIndex = 30
         Me.cmdCheckUpd.Text = "Check now"
-        Me.cmdCheckUpd.UseVisualStyleBackColor = False
+        Me.cmdCheckUpd.UseVisualStyleBackColor = True
+        '
+        'chkdisablerandom
+        '
+        Me.chkdisablerandom.AutoSize = True
+        Me.chkdisablerandom.Location = New System.Drawing.Point(6, 42)
+        Me.chkdisablerandom.Name = "chkdisablerandom"
+        Me.chkdisablerandom.Size = New System.Drawing.Size(127, 17)
+        Me.chkdisablerandom.TabIndex = 28
+        Me.chkdisablerandom.Text = "Disable RND security"
+        Me.chkdisablerandom.UseVisualStyleBackColor = True
+        '
+        'chklogtofile
+        '
+        Me.chklogtofile.AutoSize = True
+        Me.chklogtofile.Location = New System.Drawing.Point(6, 65)
+        Me.chklogtofile.Name = "chklogtofile"
+        Me.chklogtofile.Size = New System.Drawing.Size(72, 17)
+        Me.chklogtofile.TabIndex = 26
+        Me.chklogtofile.Text = "Log to file"
+        Me.chklogtofile.UseVisualStyleBackColor = True
+        '
+        'grpQE
+        '
+        Me.grpQE.Controls.Add(Me.chkcolourQE)
+        Me.grpQE.Location = New System.Drawing.Point(12, 151)
+        Me.grpQE.Name = "grpQE"
+        Me.grpQE.Size = New System.Drawing.Size(225, 53)
+        Me.grpQE.TabIndex = 2
+        Me.grpQE.TabStop = False
+        Me.grpQE.Text = "QE Options"
+        '
+        'chkcolourQE
+        '
+        Me.chkcolourQE.AutoSize = True
+        Me.chkcolourQE.Location = New System.Drawing.Point(6, 19)
+        Me.chkcolourQE.Name = "chkcolourQE"
+        Me.chkcolourQE.Size = New System.Drawing.Size(183, 17)
+        Me.chkcolourQE.TabIndex = 27
+        Me.chkcolourQE.Text = "Colour text in QE (To be repaired)"
+        Me.chkcolourQE.UseVisualStyleBackColor = True
+        '
+        'CmdApply
+        '
+        Me.CmdApply.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.CmdApply.Location = New System.Drawing.Point(156, 209)
+        Me.CmdApply.Name = "CmdApply"
+        Me.CmdApply.Size = New System.Drawing.Size(81, 25)
+        Me.CmdApply.TabIndex = 3
+        Me.CmdApply.Text = "Apply"
+        Me.CmdApply.UseVisualStyleBackColor = True
+        '
+        'cmddataloc
+        '
+        Me.cmddataloc.Location = New System.Drawing.Point(12, 209)
+        Me.cmddataloc.Name = "cmddataloc"
+        Me.cmddataloc.Size = New System.Drawing.Size(81, 25)
+        Me.cmddataloc.TabIndex = 4
+        Me.cmddataloc.Text = "Data location"
+        Me.cmddataloc.UseVisualStyleBackColor = True
         '
         'frmconfig
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(249, 174)
-        Me.Controls.Add(Me.cmdCheckUpd)
-        Me.Controls.Add(Me.chkUpdates)
-        Me.Controls.Add(Me.chkdisablerandom)
-        Me.Controls.Add(Me.chkcolourQE)
-        Me.Controls.Add(Me.chklogtofile)
-        Me.Controls.Add(Me.CmdApply)
+        Me.ClientSize = New System.Drawing.Size(249, 243)
         Me.Controls.Add(Me.cmddataloc)
-        Me.Controls.Add(Me.chkautologin)
+        Me.Controls.Add(Me.CmdApply)
+        Me.Controls.Add(Me.grpQE)
+        Me.Controls.Add(Me.grpSettings)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "frmconfig"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Configuration"
+        Me.grpSettings.ResumeLayout(False)
+        Me.grpSettings.PerformLayout()
+        Me.grpQE.ResumeLayout(False)
+        Me.grpQE.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents chkautologin As CheckBox
-    Public WithEvents cmddataloc As Button
-    Public WithEvents CmdApply As Button
-    Friend WithEvents chklogtofile As CheckBox
-    Friend WithEvents chkcolourQE As CheckBox
-    Friend WithEvents chkdisablerandom As CheckBox
+    Friend WithEvents grpSettings As GroupBox
     Friend WithEvents chkUpdates As CheckBox
-    Public WithEvents cmdCheckUpd As Button
+    Friend WithEvents cmdCheckUpd As Button
+    Friend WithEvents chkdisablerandom As CheckBox
+    Friend WithEvents chklogtofile As CheckBox
+    Friend WithEvents grpQE As GroupBox
+    Friend WithEvents chkcolourQE As CheckBox
+    Friend WithEvents CmdApply As Button
+    Friend WithEvents cmddataloc As Button
+
 End Class
